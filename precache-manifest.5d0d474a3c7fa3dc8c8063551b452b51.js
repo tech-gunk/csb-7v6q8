9a1b4dc84b746871d6358caeab5159a0933dc3cc
Code Sandbox <hello@codesandbox.io>
2022-01-19 15:45:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5d0d474a3c7fa3dc8c8063551b452b51.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa07227c7d9b85ecaf8cc9b57c5d2ec4",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/index.html"
+  },
+  {
+    "revision": "ce06cda5d774658c835f",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/css/2.dce10f6f.chunk.css"
+  },
+  {
+    "revision": "6575eb8b691b7425d8fb",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/css/main.829ab24e.chunk.css"
+  },
+  {
+    "revision": "ce06cda5d774658c835f",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/js/2.04b5bf18.chunk.js"
+  },
+  {
+    "revision": "edd7f59539dcdaf6da8fc925079ff7c2",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/js/2.04b5bf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6575eb8b691b7425d8fb",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/js/main.10774a7a.chunk.js"
+  },
+  {
+    "revision": "0b749c571c3333215b23",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/js/runtime-main.90f264ba.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "https://tech-gunk.github.io/csb-7v6q8/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
